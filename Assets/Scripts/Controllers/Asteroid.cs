1ff99ea35706a207b714335faa418a281133104b
versaeve <versaeve@OA-S138-03>
2025-09-19 17:52:14 -0400

    task 3 asteroids #Journal 3

--- Assets/Scripts/Controllers/Asteroid.cs
+++ Assets/Scripts/Controllers/Asteroid.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Asteroid : MonoBehaviour
 {
@@ -16,5 +17,35 @@ public class Asteroid : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        AsteroidMovement();
+    }
+
+    public void AsteroidMovement()
+    {
+        Vector3 asteroidPos = transform.position;
+        float asteroidMagnitude = MethodExamples.GetMagnitude(asteroidPos);
+
+        Vector3 random = new Vector3 (Random.Range(-18, 18), Random.Range(-18, 18));
+        Vector3.ClampMagnitude(random, maxFloatDistance);
+        Debug.Log(random);
+
+        //Vector3 randomPos = new Vector3(asteroidPos.x + maxFloatDistance, asteroidPos.y + maxFloatDistance);
+        //Debug.Log(randomPos);
+
+        //float randomMagnitude = MethodExamples.GetMagnitude(randomPos);
+        
+        Vector3 middleMan = asteroidPos - random; 
+        Vector3 middle = middleMan.normalized;
+
+        transform.position += middle * Time.deltaTime * moveSpeed;
+
+        Vector3 arrival = middleMan - asteroidPos;
+        Vector3 arrivalNormal = arrival.normalized;
+        //Vector3 middle2 = asteroidPos - arrivalDistance;
+
+        //if (asteroidPos = arrivalNormal)
+        {
+
+        }
     }
 }

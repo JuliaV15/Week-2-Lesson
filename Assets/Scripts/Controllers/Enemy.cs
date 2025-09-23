6942b3722e9548f29fc501db8ebaa46588774adf
JULIAPC\Julia <juliaversaevel@gmail.com>
2025-09-22 20:12:26 -0400

    task 2 - enemy movement #Journal 3

--- Assets/Scripts/Controllers/Enemy.cs
+++ Assets/Scripts/Controllers/Enemy.cs
@@ -1,10 +1,25 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Enemy : MonoBehaviour
 {
+    public Transform player;
+    public float enemyDistance;
+    public float enemySpeed;
+
     private void Update()
     {
+        EnemyMovement();
+    }
+
+    public void EnemyMovement()
+    {
+        Vector3 playerPos = player.transform.position;
+        Vector3 enemyPos = new Vector3 (playerPos.x + enemyDistance, playerPos.y + enemyDistance);
+
+        transform.position += playerPos * Time.deltaTime * enemySpeed/2;
     }
 
 }

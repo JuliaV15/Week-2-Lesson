dab3c93d7af1daf551027aa6b17d87e77fabf664
JULIAPC\Julia <juliaversaevel@gmail.com>
2025-10-16 22:12:44 -0400

    in the new player movement script i changed it to the acceleration and deceleration script keely provided because I could not get the deceleration working, and the way he did it was based off the way he did his acceleration.

--- Assets/Scripts/SS Final/Player Movement.cs
+++ Assets/Scripts/SS Final/Player Movement.cs
@@ -5,12 +5,16 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     public float accelerationTime = 3.0f;
-    private Vector3 velocity = Vector3.zero;
+    private Vector3 velocity;
+    public float acceleration;
+    public float decelerationTime = 3.0f;
+    public float deceleration;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        acceleration = speed / accelerationTime;
+        deceleration = speed / accelerationTime;
     }
 
     // Update is called once per frame
@@ -21,31 +25,61 @@ public class PlayerMovement : MonoBehaviour
 
     public void playerMove()
     {
-        float acceleration = speed / accelerationTime;
+        Vector2 playerInput = Vector2.zero;
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            velocity += acceleration * Time.deltaTime * Vector3.up;
+            //velocity += acceleration * Time.deltaTime * Vector3.up;
+            playerInput += Vector2.up;
 
         }
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            velocity += acceleration * Time.deltaTime * Vector3.down;
-
+           // velocity += acceleration * Time.deltaTime * Vector3.down;
+            playerInput += Vector2.down;
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            velocity += acceleration * Time.deltaTime * Vector3.left;
+           // velocity += acceleration * Time.deltaTime * Vector3.left;
+            playerInput += Vector2.left;
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            velocity += acceleration * Time.deltaTime * Vector3.right;
+            // velocity += acceleration * Time.deltaTime * Vector3.right;
+            playerInput += Vector2.right;
         }
 
-        velocity = Vector3.ClampMagnitude(velocity, speed);
+       // velocity = Vector3.ClampMagnitude(velocity, speed);
+       // transform.position += velocity * Time.deltaTime;
+
+        if (playerInput.magnitude > 0)
+        {
+            velocity += (Vector3)playerInput.normalized * acceleration * Time.deltaTime;
+
+            if (velocity.magnitude > speed)
+            {
+                velocity = velocity.normalized * speed;
+            }
+        }
+        else
+        {
+            Vector3 velocityChange = velocity.normalized * deceleration * Time.deltaTime; // storing the change in velocity using vector3
+
+            if (velocityChange.magnitude > velocity.magnitude) //is the magnitude of velocity change bigger than magnitude of velocity
+            {
+                velocity = Vector3.zero; //make ship stop
+            }
+            else
+            {
+                velocity -= velocityChange; //if not, then we will do the velocity subtraction
+            }
+
+           // velocity -= velocity.normalized* deceleration *Time.deltaTime; we did not use this because if we stop pushing the arrow keys then the ship will start to go in the opposite direction
+        }
+
         transform.position += velocity * Time.deltaTime;
     }
 }
